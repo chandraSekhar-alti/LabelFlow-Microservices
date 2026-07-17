720ee883dc366009cfed437c800a9318a2ce71fd
chandrasekhar-alti <althichandu123@gmail.com>
2026-07-18 00:32:08 +0530

    first commit

---
+++ labelchange-service/src/main/java/com/labelflow/labelchange/LabelchangeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.labelflow.labelchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelchangeServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LabelchangeServiceApplication.class, args);
+  }
+}
---
+++ labelchange-service/src/test/java/com/labelflow/labelchange/LabelchangeServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.labelflow.labelchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelchangeServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
